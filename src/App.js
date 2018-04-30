c408f2c21f6e2a0d201a8b5691e9f04e1a741dd4
vkakde <vishwajeet.kakde@gmail.com>
2018-04-30 13:43:34 -0400

    Added preamble remarks for authentication steps in App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+/*
+Reddit API: 
+
+Authentication for Reddit API requests
+1. URL: https://github.com/reddit-archive/reddit/wiki/OAuth2
+2. For API requests not requiring confidential user data from reddit, refer "Application only OAuth"
+   section of wiki at above URL.
+*/
+
+
+
+
 import React, { Component } from 'react';
 class App extends Component {
   constructor(){
