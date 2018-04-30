c4dcc2dad617e61a9269261f605152397ca1f8d5
vkakde <vishwajeet.kakde@gmail.com>
2018-04-30 15:10:26 -0400

    Added search bar to search for subreddit. Add subreddit search API call in App.js>handleSearchSubreddit()

--- src/App.js
+++ src/App.js
@@ -7,30 +7,30 @@ Authentication for Reddit API requests
    section of wiki at above URL.
 */
 
-
 import React, { Component } from 'react';
+import SearchBar from './Components/SearchBar';
+
 var axios = require("axios");
-var expiresIn=0;
+var expiresIn = 0;
 var accessToken = '';
-var accessTokenTimestamp=0;
+var accessTokenTimestamp = 0;
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
+      accessToken
     };
   }
 
-  componentWillMount(){
+  componentWillMount() {
   }
 
-  componentDidMount(){
-    ///\cite https://github.com/spotify/web-api/issues/321
-    if(accessTokenTimestamp === 0 || Date.now() - accessTokenTimestamp <= 100){
+  componentDidMount() {
+    if (accessTokenTimestamp === 0 || Date.now() - accessTokenTimestamp <= 100) {
       axios.request({
         url: "https://www.reddit.com/api/v1/access_token",
         method: "post",
-        //baseURL: "https://cs-554-spotify-proxy.herokuapp.com/api",
         auth: {
           ///\remark Client ID and secret belong to dev account of reddit username: cs554acc1
           username: "BGW4aLakJfAdmQ",
@@ -40,18 +40,23 @@ class App extends Component {
           "grant_type": "client_credentials",
           "scope": "public"
         }
-      }).then(function(res) {
+      }).then(function (res) {
         expiresIn = res.data.expires_in;
         accessToken = res.data.access_token;
         accessTokenTimestamp = Date.now();
-        console.log("Access Token acquired successfully. Expires in: "+expiresIn+"\nToken timestamp: "+accessTokenTimestamp);
+        console.log("Access Token acquired successfully. Expires in: " + expiresIn + "\nToken timestamp: " + accessTokenTimestamp);
       });
     }
+  }
+
+  handleSearchSubreddit(searchQuery) {
+    console.log("\nhandleSearchSubreddit!");
   }
 
   render() {
     return (
       <div className="container">
+        <SearchBar searchSubreddit={this.handleSearchSubreddit.bind(this)} />
       </div>
     );
   }
