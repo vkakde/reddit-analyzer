1936f2fde498e83a20a196ef8b9ee227fb40a2b5
vkakde <vishwajeet.kakde@gmail.com>
2018-04-30 13:53:17 -0400

    Import axios in App,js, and added axios as a dependency in package.json

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ Authentication for Reddit API requests
 */
 
 
-
-
 import React, { Component } from 'react';
+var axios = require("axios");
+var expiresIn=0;
+var accessToken = '';
+var accessTokenTimestamp=0;
+
 class App extends Component {
   constructor(){
     super();
@@ -22,6 +25,28 @@ class App extends Component {
   }
 
   componentDidMount(){
+    ///\cite https://github.com/spotify/web-api/issues/321
+    if(accessTokenTimestamp === 0 || Date.now() - accessTokenTimestamp <= 100){
+      axios.request({
+        url: "https://www.reddit.com/api/v1/access_token",
+        method: "post",
+        //baseURL: "https://cs-554-spotify-proxy.herokuapp.com/api",
+        auth: {
+          ///\remark Client ID and secret belong to dev account of reddit username: cs554acc1
+          username: "BGW4aLakJfAdmQ",
+          password: "jhZ8Amx4HTmXdZjDGodRFdZhlwQ"
+        },
+        data: {
+          "grant_type": "client_credentials",
+          "scope": "public"
+        }
+      }).then(function(res) {
+        expiresIn = res.data.expires_in;
+        accessToken = res.data.access_token;
+        accessTokenTimestamp = Date.now();
+        console.log("Access Token acquired successfully. Expires in: "+expiresIn+"\nToken timestamp: "+accessTokenTimestamp);
+      });
+    }
   }
 
   render() {
