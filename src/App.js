9aafb27e80b3218438b97a376f0a56e55814c804
vkakde <vishwajeet.kakde@gmail.com>
2018-04-30 13:26:51 -0400

    Created vanila npm react app with package.json

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+    };
+  }
+
+  componentWillMount(){
+  }
+
+  componentDidMount(){
+  }
+
+  render() {
+    return (
+      <div className="container">
+      </div>
+    );
+  }
+}
+
+export default App;
